7f2b3f4add37dbef0e0380b3e1753f6f4cb89079
Dvdrepetto <davidmrepetto@gmail.com>
2025-01-23 21:26:19 +0200

    solved lab

--- src/index.js
+++ src/index.js
@@ -8,15 +8,39 @@ let timer = null; // Variable to store the interval
 
 // Your code goes here ...
 
-
-
+const btnStart = document.getElementById('start-btn');
+btnStart.addEventListener('click', () => {
+  startCountdown();
+  setTimeout(() => {
+    btnStart.disabled = true;
+  }, 100);
+  setTimeout(() => {
+    btnStart.disabled = false;
+  }, 11500);
+});
 
 // ITERATION 2: Start Countdown
+
 function startCountdown() {
   console.log("startCountdown called!");
-
-
   // Your code goes here ...
+
+  const time = document.getElementById('time')
+  let count = 10;
+
+  const newTimer = setInterval(function () {
+    time.textContent = `${count}`;
+    count--;
+    if (count === 9) {
+      showToast("⏰ Final countdown! ⏰");
+    } else if (count === 5) {
+      showToast("Start the engines! 💥");
+    } else if (count < 0) {
+      clearInterval(newTimer); 
+      console.log('counter finish');
+      showToast("Lift off! 🚀");
+    }
+  }, 1000);
 }
 
 
@@ -27,12 +51,20 @@ function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
-
-
-
+const toast = document.querySelector('#toast');
+toast.textContent = message;
+toast.classList.add('show');
+setTimeout(() => {
+  toast.classList.remove('show');
+}, 3000);
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
+document.getElementById('close-toast');
+toast.addEventListener('click',function(){
+  console.log("aqui")
+toast.classList.remove('show')
+});
 
 }
